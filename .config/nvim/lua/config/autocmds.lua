0f612bc4d92d1e9f735dc4a4d1115659cf1f4170
Thomas Chartron <thomas.chartron@gmail.com>
2024-05-10 16:49:58 +0200

    Update autocmds.lua

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,20 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+_G.toggle_line_numbering = function()
+  if vim.wo.number and vim.fn.mode() ~= "i" then
+    vim.wo.relativenumber = true
+  else
+    vim.wo.relativenumber = false
+  end
+end
+
+-- Set up autocmd using Lua
+vim.cmd([[
+  augroup numbertoggle
+    autocmd!
+    autocmd BufEnter,FocusGained,InsertLeave,WinEnter * lua toggle_line_numbering()
+    autocmd BufLeave,FocusLost,InsertEnter,WinLeave * set nornu
+  augroup END
+]])
