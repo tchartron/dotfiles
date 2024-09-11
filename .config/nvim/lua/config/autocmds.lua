7d0a1f6ed915159b2587ecbfb7ea6fed576cde2c
Thomas Chartron <thomas.chartron@gmail.com>
2024-09-11 17:03:26 +0200

    Update autocmds.lua

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,6 +1,7 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+-- Auto toggles relative and absolute line numbers depending on vim current mode
 
 _G.toggle_line_numbering = function()
   if vim.wo.number and vim.fn.mode() ~= "i" then
